264bfa05f7b5d50df3cf7d390fbf4a6468c69e44
ellreka <ellreka5364@gmail.com>
2023-06-03 01:20:34 +0900

    feat: add config.fish

--- fish/abbreviations.fish
+++ fish/abbreviations.fish
@@ -1,39 +1,39 @@
 # git
-abbr -a -U -- ga 'git add .'
+abbr -a ga 'git add .'
 
-abbr -a -U -- gp 'git push origin (git rev-parse --abbrev-ref HEAD)'
-abbr -a -U -- gpf 'git push origin (git rev-parse --abbrev-ref HEAD) --force-with-lease'
-abbr -a -U -- gta 'git push origin (git rev-parse --abbrev-ref HEAD) && git push origin (git describe --tags)'
+abbr -a gp 'git push origin (git rev-parse --abbrev-ref HEAD)'
+abbr -a gpf 'git push origin (git rev-parse --abbrev-ref HEAD) --force-with-lease'
+abbr -a gta 'git push origin (git rev-parse --abbrev-ref HEAD) && git push origin (git describe --tags)'
 
-abbr -a -U -- gpl 'git pull origin (git rev-parse --abbrev-ref HEAD)'
+abbr -a gpl 'git pull origin (git rev-parse --abbrev-ref HEAD)'
 
-abbr -a -U -- gl 'git log --oneline'
+abbr -a gl 'git log --oneline'
 
-abbr -a -U -- gc 'git commit -m'
-abbr -a -U -- gfeat 'git commit -m "feat:'
-abbr -a -U -- gfix 'git commit -m "fix:'
-abbr -a -U -- gchore 'git commit -m "chore:'
+abbr -a gc 'git commit -m'
+abbr -a gfeat 'git commit -m "feat:'
+abbr -a gfix 'git commit -m "fix:'
+abbr -a gchore 'git commit -m "chore:'
 
-abbr -a -U -- gs 'git switch'
-abbr -a -U -- gsd 'git switch develop'
-abbr -a -U -- gsm git\ switch\ \(git\ remote\ show\ origin\ \|\ grep\ \'HEAD\ branch\'\ \|\ awk\ \'\{print\ \$NF\}\'\)
-abbr -a -U -- gsc 'git switch -c'
-abbr -a -U -- gca 'git commit --amend'
+abbr -a gs 'git switch'
+abbr -a gsd 'git switch develop'
+abbr -a gsm git\ switch\ \(git\ remote\ show\ origin\ \|\ grep\ \'HEAD\ branch\'\ \|\ awk\ \'\{print\ \$NF\}\'\)
+abbr -a gsc 'git switch -c'
+abbr -a gca 'git commit --amend'
 
-abbr -a -U -- gst 'git stash -u'
-abbr -a -U -- gsa 'git stash apply'
+abbr -a gst 'git stash -u'
+abbr -a gsa 'git stash apply'
 
-abbr -a -U -- gsf 'git branch -a | fzf | xargs git switch'
+abbr -a gsf 'git branch -a | fzf | xargs git switch'
 
-abbr -a -U -- ghpr 'gh pr checkout'
+abbr -a ghpr 'gh pr checkout'
 
-abbr -a -U -- rmn 'rm -rf node_modules && narn'
-abbr -a -U -- rmnode 'rm -rf node_modules'
+abbr -a rmn 'rm -rf node_modules && narn'
+abbr -a rmnode 'rm -rf node_modules'
 
 # yarn,npm,pnpm
 # https://github.com/antfu/ni
-abbr -a -U -- y 'ni'
-abbr -a -U -- ya 'ni -D'
-abbr -a -U -- yd 'nr dev'
-abbr -a -U -- yu 'nu'
-abbr -a -U -- yui 'nu -i'
+abbr -a y 'ni'
+abbr -a ya 'ni -D'
+abbr -a yd 'nr dev'
+abbr -a yu 'nu'
+abbr -a yui 'nu -i'
---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+if not status --is-interactive
+  exit 0
+end
+
+function abbr_set --description 'set abbreviations'
+  echo "setting fish abbreviations"
+  # first erase all existing abbreviations
+  for ab in (abbr --list)
+    abbr --erase "$ab"
+  end
+
+  # then source current abbreviations
+  source "$HOME/.config/fish/abbreviations.fish"
+  set -U fish_abbreviations_set true
+end
+
+abbr_set
